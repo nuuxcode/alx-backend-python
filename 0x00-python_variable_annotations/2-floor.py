a6cad46b1386bb78c1e209af0e87247ba009d5c9
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-01-11 22:25:25 +0100

    🥎🎩👒 Task2 Basic annotations - floor

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ Module documentation """
+import math
 
 
-def add(a: float, b: float) -> float:
+def floor(n: float) -> int:
     """Func doc"""
-    return a + b
+    return math.floor(n)
