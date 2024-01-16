c14a82fd4bfd8d7007da117fc77ba1d4ceb18a16
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-01-16 16:00:03 +0100

    💖💖💖 Task1 Async Comprehensions

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """ Module documentation """
-from typing import Generator
+from typing import List
 
 async_generator = __import__("0-async_generator").async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """Func doc"""
     result = []
     async for i in async_generator():
