a2931a3d37d65c378bf2f037f979f4eddfe4d595
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-01-16 22:34:17 +0100

    test

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -9,9 +9,11 @@ async_comprehension = __import__("1-async_comprehension").async_comprehension
 async def measure_runtime() -> float:
     """Function documentation"""
     start = time.perf_counter()
-    coroutines = []
-    for i in range(4):
-        coroutines.append(async_comprehension())
-    await asyncio.gather(*coroutines)
+    result = await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
     end = time.perf_counter()
     return end - start
