e2520d436431a74d8e3d8f1a9b6a64de36d2be91
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-02-05 21:10:32 +0100

    ☺😚😙 Task1 Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -21,3 +21,11 @@ class TestAccessNestedMap(unittest.TestCase):
     ) -> None:
         """doc doc doc"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([({}, ("a",)), ({"a": 1}, ("a", "b"))])
+    def test_access_nested_map_exception(
+        self, nested_map: Dict[str, Any], path: Tuple[str]
+    ) -> None:
+        """doc doc doc"""
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
